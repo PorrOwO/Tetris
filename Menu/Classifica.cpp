81a28fd099e123faa9d1657aca50c71df1ef99e1
elena <elena@localhost.localdomain>
2024-03-08 21:00:19 +0100

    aggiunto metodo aggiorna

--- Menu/Classifica.cpp
+++ Menu/Classifica.cpp
@@ -1,48 +1,90 @@
 #include "Tetris.hpp"
 
-Classifica::Classifica()
+void Punteggio::setPosition(int x, int y)
 {
-    this->winC=nullptr;
-    this->testa=nullptr;
+  this->posX=x;
+  this->posY=y;
 };
 
-Classifica::Classifica(WINDOW* win)
+
+Classifica::Classifica()
 {
-    this->winC=win;
-    this->testa=NULL;
-    this->testa=fromFileToList(testa);
+    this->win = Window();
+    this->testa=fromFileToList(NULL);
 };
+
+Classifica::Classifica(Window win)
+{
+    this->win = win;
+    this->testa=NULL;
+    this->testa=fromFileToList(NULL);
+};
+
+void Classifica::setPointsPosition(){ //RIFARE
+  int X=this->win.getWin()->_maxx;
+  int Y=this->win.getWin()->_maxy;
+  Punteggio* tmp=this->testa;
+  //setto i primi 3 punteggi come se stessero su un podio
+  if(tmp!=NULL)
+{ tmp->posX= (X/2);
+   tmp->posY=4;
+  tmp=tmp->next;
+
+    if(tmp!=NULL)
+    {
+      tmp->posX= (X/3);
+      tmp->posY=6;
+      tmp=tmp->next;
+
+      if(tmp!=NULL)
+      { tmp->posX= 2*(X/3);
+        tmp->posY=10;
+        tmp=tmp->next;
+        int i=12;
+        while(tmp!=NULL)
+          { tmp->posX= (X/2);
+            tmp->posY=i;
+            i=i+2;
+          }
+      }
+    }
+  }
+}
 
 int Classifica:: Mostra() //stampo i punteggi dal file
 {  int q; //variabile per uscire dalla classifica
   ifstream input; //dichiaro un tipo
   input.open("classifica.txt");
           string linea;
-          int Y=4;
-          int X=50;
+           int Y=4;
+          int X=this->win.getWin()->_maxx;
+
+          //setPointsPosition();
 
            while(!input.eof()) //si esce da questo ciclo quando il file termina (eof=end of file)
            {
              getline(input,linea);
-             mvwprintw(this->winC,Y,X,linea.c_str());
+             mvwprintw(this->win.getWin(),Y,X/2-(linea.length())/2-linea.length()%2,linea.c_str());
              Y++;
             }
+
             input.close();
-    wrefresh(this->winC);
+    wrefresh(this->win.getWin());
     do{
-        wattron(this->winC, A_STANDOUT);
-            mvwprintw(this->winC,1,1,"esci");
-            wattroff(this->winC, A_STANDOUT);
-    q=wgetch(this->winC);
+        wattron(this->win.getWin(), A_STANDOUT);
+            mvwprintw(this->win.getWin(),1,1,"esci");
+            wattroff(this->win.getWin(), A_STANDOUT);
+    q=wgetch(this->win.getWin());
     } while(q!=10);//esco dalla classifica solo quando l'utente preme enter
-    wclear(this->winC);
+    wclear(this->win.getWin());
     refresh();
     return -1;
 };
 
 
-Punteggio* Classifica:: fromFileToList(Punteggio* head) //da sistemare
-{ ifstream input;
+Punteggio* Classifica:: fromFileToList(Punteggio* head)
+{
+  ifstream input;
   string linea;
   input.open("classifica.txt");
   Punteggio* tmp=NULL;
@@ -53,7 +95,7 @@ Punteggio* Classifica:: fromFileToList(Punteggio* head) //da sistemare
   if(head==NULL) //inserimento di una linea in una lista vuota
 {
   head=new Punteggio;
-  head->p=linea; // 10 è la base
+  head->p=linea;
   head->next=NULL;
   tmp=head;
 }
@@ -71,4 +113,51 @@ else  //inserimento di una linea in una lista non vuota
 }
   return head;
 };
+
+
+Punteggio* Classifica::sortInsert(int points, Punteggio* head)
+{
+  if(head==NULL)
+  {head= new Punteggio;
+    head->p=to_string(points);
+    head->next=NULL;
+    return head;
+
+  }
+  else
+  {
+    if(points>=stoi(head->p,0,10) )//inserimento del punteggio in testa alla lista
+    {
+      Punteggio* tmp= new Punteggio;
+      tmp->next=head;
+      tmp->p=to_string(points);
+      head=tmp;
+      return head;
+    }
+    else //punteggio è minore di quello in testa, va inserito in mezzo alla lista
+    {
+      head->next=sortInsert(points,head->next);
+      return head;
+    }
+  }
+};
+
+void Classifica::Aggiorna(int points)
+{ ofstream output; //output su file
+  this->testa=sortInsert(points,testa);
+  output.open("classifica.txt");
+
+  while(testa!=NULL)
+  { output<<testa->p<<endl;
+    testa=testa->next;
+
+  }
+  output.close();
+
+
+};
+
+
+
+
 
