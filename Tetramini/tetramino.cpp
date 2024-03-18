e1f07714624e8e44b203d25357fdc8ba8ee1615d
PorrOwO <andrea.dalcin04@gmail.com>
2024-03-18 11:01:30 +0100

    add rotation, and draw tetrominos

---
+++ Tetramini/tetramino.cpp
@@ -0,0 +1,11 @@
+#include "Tetramino.hpp"
+
+
+Tetramino::Tetramino(char* tipo)
+{
+    this->type[0].append(tipo);
+}
+
+wstring Tetramino::getRngType(){
+    
+}
---
+++ constants.hpp
@@ -0,0 +1,15 @@
+//forme dei tetramini
+#define SHAPE_0                 "..#...#...#...#."
+#define SHAPE_1                 ".##..#...#......"
+#define SHAPE_2                 ".#...#...##....."
+#define SHAPE_3                 "..#..##..#......"
+#define SHAPE_4                 ".#...##...#....."
+#define SHAPE_5                 ".....##..##....."
+#define SHAPE_6                 ".#...##..#......"
+
+//dimensioni finestra
+#define W_WIDTH 40
+#define W_HEIGHT 40
+
+//colori
+#define RED 1
