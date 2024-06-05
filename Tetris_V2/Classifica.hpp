4b0c6af589e1172d04bdf3863d4da9c33bb91d9b
Elenabartx <elena.bartolotta@studio.unibo.it>
2024-06-05 16:44:12 +0200

    "gen changes"

--- Tetris_V2/Classifica.hpp
+++ Tetris_V2/Classifica.hpp
@@ -3,15 +3,12 @@
 
 struct Punteggio{
     std::string p;
-    int posX;
-    int posY;
     Punteggio* next; 
 };
 
 class Classifica{
 protected:
     WINDOW* win;
-    //int numPunteggi=10; voglio stampare solo i primi  10 punteggi più alti
     Punteggio* testa; //puntatore alla testa della lista di punteggi
 public:
    
@@ -19,8 +16,6 @@ public:
     Classifica(WINDOW* win);
    
     int Mostra(); //ritorna valore per uscire da schermata classifica
-
-    //funzioni ausiliarie per l'aggiornamento della classifica VANNO TENUTE COME METODI O MEGLIO LIBRERIA?
     Punteggio* fromFileToList(Punteggio* head); //necessaria per aggiornare la classifica
     Punteggio* sortInsert(int points, Punteggio* head); //inserimento del punteggio in una lista ordinata
     void Aggiorna(int points); //aggiorna file di testo con il nuovo punteggio
